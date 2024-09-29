2c3d0831dd19fed37330c7eddc51793360446d88
Ankan Bera <beraankan8@gmail.com>
2024-09-29 13:27:07 +0530

    blog

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-function Blog() {
+const Blog: React.FC = () => {
   return (
-    <div>
-      Blog
+    <div className="blog-page bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 min-h-screen flex flex-col items-center justify-center text-white">
+      <header className="text-center py-12">
+        <h1 className="text-6xl font-bold mb-4">COMING SOON . . .</h1>
+        <p className="text-lg">
+          Stay tuned for updates
+        </p>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
